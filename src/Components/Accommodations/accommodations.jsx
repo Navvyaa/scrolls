08f083e24503b26368de56e78edff3c2d1736665
Abhinandan <abhinandnn21@gmail.com>
2024-04-04 14:20:12 +0530

    updated rules and accommodation content

--- src/Components/Accommodations/accommodations.jsx
+++ src/Components/Accommodations/accommodations.jsx
@@ -9,6 +9,7 @@ function Accommodations() {
         <Navbar/>
         <div className='accommodations'>
         <p className="dashboard" id="update">Accommodation</p>
+        <div className='font-bold'>Participants from distant locations will receive free accommodation and meals upon verification. This ensures their comfort and convenience during the event, removing any logistical barriers to their participation.</div>
             </div>
             <Footer />
         <ToastContainer />
